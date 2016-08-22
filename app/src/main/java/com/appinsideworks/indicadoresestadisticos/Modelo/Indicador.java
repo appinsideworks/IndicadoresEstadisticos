a0e1978a5a02689cbe153db933cc21d5625bf495
Ramón Álvarez Baeza <ab.ramon1109@gmail.com>
2016-08-22 13:27:26 -0500

    Made Indicador a Parcelable Object

--- app/src/main/java/com/appinsideworks/indicadoresestadisticos/Modelo/Indicador.java
+++ app/src/main/java/com/appinsideworks/indicadoresestadisticos/Modelo/Indicador.java
@@ -1,15 +1,20 @@
 package com.appinsideworks.indicadoresestadisticos.Modelo;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
 /**
  * Created by ramon_a on 8/8/16.
  */
-public class Indicador {
-
+public class Indicador implements Parcelable {
+    //Atributos
     private int desglose_geografico;
     private int indicador;
     private String nombre;
     private Boolean ver_areas;
 
+    //Constructores
     public Indicador(int desglose_geografico, int indicador, String nombre, Boolean ver_areas) {
         this.desglose_geografico = desglose_geografico;
         this.indicador = indicador;
@@ -17,6 +22,13 @@ public class Indicador {
         this.ver_areas = ver_areas;
     }
 
+    public Indicador(Parcel parcel) {
+        readFromParcel(parcel);
+    }
+
+    //Fin
+
+    //Getters y Setters
     public int getDesglose_geografico() {
         return desglose_geografico;
     }
@@ -49,8 +61,53 @@ public class Indicador {
         this.ver_areas = ver_areas;
     }
 
+    //Fin
+
+
+    //ToString
     @Override
     public String toString() {
         return "Indicador: " + indicador + "\nNombre: " + nombre + "\nDesglose geográfico: " + desglose_geografico + "\nVer áreas: " + ver_areas;
     }
+
+
+    //Parcelable
+
+    public static final Parcelable.Creator<Indicador> CREATOR = new Creator<Indicador>() {
+
+        @Override
+        public Indicador[] newArray(int i) {
+            return new Indicador[i];
+        }
+
+        @Override
+        public Indicador createFromParcel(Parcel parcel) {
+            return new Indicador(parcel);
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(desglose_geografico);
+        parcel.writeInt(indicador);
+        parcel.writeString(nombre);
+        parcel.writeBooleanArray(new boolean[]{ver_areas});
+
+    }
+
+    private void readFromParcel(Parcel in) {
+        this.desglose_geografico = in.readInt();
+        this.indicador = in.readInt();
+        this.nombre = in.readString();
+
+        boolean[] aux = new boolean[1];
+        in.readBooleanArray(aux);
+        this.ver_areas = aux[0];
+    }
+    //Fin
 }
